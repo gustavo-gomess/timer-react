17bf62c29e71a7991bb1596b426ebefb017a368c
Gustavo Henrique Silveira Go <gustavo.gomes@ema.net.br>
2022-12-12 17:15:48 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+ import Formulario from './components/formulario';
+import Lista from './components/lista';
+
+function App() {
+  return (
+    <div className='App'>
+      <Formulario />
+      <Lista />
+    </div>
+  )
+    
+}
+
+export default App;
